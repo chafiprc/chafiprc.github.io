d039c07981e93963a8b933f545680b4ec6515ac8
chafiprc <1538153895@qq.com>
2023-07-28 19:38:42 +0800

    Site updated: 2023-07-28 19:38:41

--- js/jumpNum.js
+++ js/jumpNum.js
@@ -1,14 +1,15 @@
 (function () {
   let jumpNum = {
     tag: "jumpNum", //html标签名称
-    speed: 50,
-    minTime: 500,
-    maxTime: 5000,
-    maxTimeOut: 500,
+    speed: 50, //刷新速度
+    minTime: 500, //最小显示时间
+    maxTime: 5000, //最大显示时间
+    maxTimeOut: 500, //获取数字的最大超时时间
     sleep: function (ms) {
       return new Promise((resolve) => setTimeout(resolve, ms));
     },
     getNum: function (eachJNE) {
+      // 获取数字
       let getNumTimes = 0;
       let attemptTime = ~~(jumpNum.maxTimeOut / jumpNum.speed);
       while (getNumTimes <= attemptTime) {
@@ -24,6 +25,7 @@
       throw "Can't get number.";
     },
     executeJumpNum: async function (eachJNE, duration) {
+      //异步执行
       let num = Number(); // 显示的数
       let step = Number(); //步长
       let temp = Number(); //每一步执行后的数
@@ -31,26 +33,59 @@
       try {
         num = jumpNum.getNum(eachJNE);
         temp = 0;
-        eachJNE.innerText = String(num);
-        step = ~~(num / times);
-        for (let i = 0; i < times; i++) {
-          temp += step;
-          step = ~~((num - temp) / (times - i - 1)); //更新步长，使步长更为均匀
+        eachJNE.innerText = String(temp);
+        if (Number.isInteger(num)) {
+          //整数判断
+          step = ~~(num / times);
+          for (let i = 0; i < times; i++) {
+            temp += step;
+            step = ~~((num - temp) / (times - i - 1)); //更新步长，使步长更为均匀
+            await jumpNum.sleep(jumpNum.speed);
+            eachJNE.innerText = String(temp);
+          }
           await jumpNum.sleep(jumpNum.speed);
-          eachJNE.innerText = String(temp);
-          //console.log(temp);
+          eachJNE.innerText = String(num);
+        } else {
+          //小数判断
+          let intNum = Math.trunc(num);
+          let intNumLen = String(intNum).length;
+          let decNum = Number(String(num).slice(intNumLen + 1));
+          let decNumLen = String(decNum).length;
+          let intTimes = ~~((times * intNumLen) / (intNumLen + decNumLen));
+          let decTimes = times - intTimes;
+          step = ~~(intNum / intTimes);
+          for (let i = 0; i < intTimes; i++) {
+            temp += step;
+            step = ~~((intNum - temp) / (intTimes - i - 1)); //更新步长，使步长更为均匀
+            await jumpNum.sleep(jumpNum.speed);
+            eachJNE.innerText = String(temp);
+            console.log(eachJNE.innerText);
+          }
+          let decTemp = 0;
+          temp = intNum;
+          step = ~~(decNum / decTimes);
+          for (let i = 0; i < decTimes; i++) {
+            decTemp += step;
+            temp = Number(String(intNum) + "." + String(decTemp));
+            step = ~~((decNum - decTemp) / (decTimes - i - 1)); //更新步长，使步长更为均匀
+            await jumpNum.sleep(jumpNum.speed);
+            eachJNE.innerText = String(temp);
+            console.log(eachJNE.innerText);
+          }
+          await jumpNum.sleep(jumpNum.speed);
+          eachJNE.innerText = String(num);
         }
-        await jumpNum.sleep(jumpNum.speed);
-        eachJNE.innerText = String(num);
       } catch (value) {
         console.log(value);
       }
     },
     init: function () {
-      let allEle = $(jumpNum.tag);
+      // 初始化函数
+      let allEle = $(jumpNum.tag); // 获取所有jumpNum标签元素
       for (let eachJNE of allEle) {
         let duration = Number();
         try {
+          //设置持续时间
           duration = Number(eachJNE.getAttribute("duration"));
           if (duration < jumpNum.minTime && duration > 0) {
             duration = jumpNum.minTime;
@@ -62,7 +97,7 @@
           ) {
             duration = duration;
           } else {
-            duration = 2000;
+            duration = 2000; // 默认持续时间
           }
           if (isNaN(duration)) {
             throw "Not a Number, set to default.";
@@ -70,15 +105,14 @@
         } catch {
           duration = 2000;
         }
-        console.log(duration);
-        let type = String();
+        let type = String(); //设置显示类型
         type = String(eachJNE.getAttribute("type"));
         if (type !== "hover") {
           jumpNum.executeJumpNum(eachJNE, duration);
         } else {
           $(eachJNE).mouseover(function () {
             jumpNum.executeJumpNum(eachJNE, duration);
-            $(eachJNE).off();
+            $(eachJNE).off(); //移除选择器，使数字仅变化一次
           });
         }
       }
